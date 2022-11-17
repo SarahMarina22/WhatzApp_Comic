450a691f9ee7603a961578663d0a551984fc4b25
SarahMarina22 <sndenbe22@fed.idserve.net>
2022-11-17 20:50:48 +0100

    Initial Commit

---
+++ WhatzApp_Comic/WhatzApp_ComicApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatzApp_ComicApp.swift
+//  WhatzApp_Comic
+//
+//  Created by Sarah Ndenbe on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatzApp_ComicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
